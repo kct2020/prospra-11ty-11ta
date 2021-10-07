ff1ac9589ea1c871eebe5d17d1b80a5513ce731b
Keith Taylor <58899793+kct2020@users.noreply.github.com>
2021-10-07 18:03:30 +0100

    Update .eleventy.js
    addPassthroughCopy('src/ads.txt')

--- .eleventy.js
+++ .eleventy.js
@@ -127,6 +127,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/*.jpg')
 	eleventyConfig.addPassthroughCopy('src/*.ico')
 	eleventyConfig.addPassthroughCopy('src/robots.txt')
+	eleventyConfig.addPassthroughCopy('src/ads.txt')
 	eleventyConfig.addPassthroughCopy('src/assets/images/')
 	eleventyConfig.addPassthroughCopy('src/assets/svg/')
 	eleventyConfig.addPassthroughCopy('src/assets/video/')
